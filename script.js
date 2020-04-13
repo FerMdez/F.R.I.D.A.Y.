82219328318dd87dc6e8d953467da3e8e7f63708
Fernando Méndez <45081533+FerMdez@users.noreply.github.com>
2020-04-13 18:41:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+    F.R.I.D.A.Y. (Mark 1)
+    Author: Fernando MÃ©ndez
+    Web: https://fermdez.ddns.net/
+    Twitter: @HumperCobra
+*/
+let rec;
+    if (!("webkitSpeechRecognition" in window)) {
+    	alert("Este navegador no soporta reconocimiento de voz. Por favor, use Chrome. | This browser does not support speech recognition. Please, use Chrome.");
+    }
+    else {
+    	rec = new webkitSpeechRecognition();
+    	rec.lang = "es-ES";
+    	rec.continuous = true;
+        rec.interim = true;
+        do{
+            rec.addEventListener("result", startVR);
+        } while(!"exit");
+    }
+
+document.getElementById('speak').addEventListener("click", ()=>{
+    say(document.getElementById("out").value);
+});
+
+
+/*FUNCIONES*/
+function startVR(event){
+	for (let i = event.resultIndex; i < event.results.length; i++){
+         document.getElementById('in').innerHTML = event.results[i][0].transcript;
+	}
+}
+
+function say(txt){
+    speechSynthesis.speak(new SpeechSynthesisUtterance(txt))
+}
+
+rec.start();
